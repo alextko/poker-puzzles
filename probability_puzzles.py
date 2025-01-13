ce553d51c510a08414b2ef4fff594eeb371323d0
Brad Levin <bradlevin08@gmail.com>
2025-01-12 18:48:48 -0600

    bones for validation

--- probability_puzzles.py
+++ probability_puzzles.py
@@ -25,7 +25,7 @@ class PokerQuiz:
         return cards
 
     def calculate_probabilities(self, hole_cards, community_cards=None):
-        """Calculate probabilities for different poker hands given any game state"""
+        """Calculate probabilities for different poker hands"""
         probabilities = {}
         
         # Reset deck and remove known cards
@@ -36,7 +36,42 @@ class PokerQuiz:
             for card in community_cards:
                 self.deck.remove(card)
 
-        all_cards = hole_cards + (community_cards if community_cards else [])
+        if community_cards:
+            all_cards = hole_cards + community_cards
+            current_ranks = [card[0] for card in all_cards]
+            current_suits = [card[1] for card in all_cards]
+            rank_counts = defaultdict(int)
+            suit_counts = defaultdict(int)
+            for rank in current_ranks:
+                rank_counts[rank] += 1
+            for suit in current_suits:
+                suit_counts[suit] += 1
+            self.calculate_post_flop_probabilities(probabilities, all_cards, rank_counts, suit_counts)
+        else:
+            self.calculate_pre_flop_probabilities(probabilities, hole_cards)
+
+        return probabilities
+
+    # def calculate_pre_flop_probabilities(self, probabilities={}, hole_cards):
+    #     """Calculate probability of hitting a pair on the flop"""
+    #     rank1, rank2 = hole_cards[0][0], hole_cards[1][0]
+        
+    #     if rank1 != rank2:
+    #         num_outs = 6
+    #         cum_prob = 1 
+    #         for i in range(3):
+    #             cum_prob *=  (52-len(hole_cards)- num_outs-i)/(52 - len(hole_cards)-i)
+    #         probabilities['Pair'] = round((1-cum_prob) * 100, 2)
+    #     else:
+    #         probabilities['Pair'] = 100 # 100% chance of a pair
+        
+    #     return probabilities
+
+    def calculate_post_flop_probabilities(self, probabilities, hole_cards, community_cards):
+        """Calculate probabilities of making hands by the river using at least one hole card"""
+        hole_ranks = [card[0] for card in hole_cards]
+        hole_suits = [card[1] for card in hole_cards]
+        all_cards = hole_cards + community_cards
         current_ranks = [card[0] for card in all_cards]
         current_suits = [card[1] for card in all_cards]
         rank_counts = defaultdict(int)
@@ -45,40 +80,12 @@ class PokerQuiz:
             rank_counts[rank] += 1
         for suit in current_suits:
             suit_counts[suit] += 1
-
-        # Determine the stage of the game
-        num_community_cards = len(community_cards) if community_cards else 0
-        if num_community_cards == 0:
-            self.calculate_pre_flop_probabilities(probabilities, hole_cards)
-        elif num_community_cards in [3, 4, 5]:
-            self.calculate_post_flop_probabilities(probabilities, all_cards, rank_counts, suit_counts, num_community_cards)
-        
-        return probabilities
-
-    def calculate_pre_flop_probabilities(self, probabilities, hole_cards):
-        """Calculate probability of hitting a pair on the flop"""
-        rank1, rank2 = hole_cards[0][0], hole_cards[1][0]
-        
-        if rank1 != rank2:
-            num_outs = 6
-            cum_prob = 1 
-            for i in range(3):
-                cum_prob *=  (52-len(hole_cards)- num_outs-i)/(52 - len(hole_cards)-i)
-            
-            probabilities['Pair'] = round((1-cum_prob) * 100, 2)
-
-    def calculate_post_flop_probabilities(self, probabilities, all_cards, rank_counts, suit_counts, num_community_cards):
-        """Calculate probabilities of making hands by the river using at least one hole card"""
-        hole_cards = all_cards[:2]  # First two cards are hole cards
-        hole_ranks = [card[0] for card in hole_cards]
-        hole_suits = [card[1] for card in hole_cards]
-        
         # Pair calculation (only counting pairs using hole cards)
         unpaired_hole_cards = [rank for rank in hole_ranks if rank_counts[rank] == 1]
         if unpaired_hole_cards:
             num_outs = sum(1 for card in self.deck if card[0] in unpaired_hole_cards)
             cum_prob = 1
-            for i in range(5 - num_community_cards):
+            for i in range(2):
                 cum_prob *= (47 - num_outs - i)/(47 - i)
             probabilities['Pair'] = round((1 - cum_prob) * 100, 2)
         
@@ -89,17 +96,17 @@ class PokerQuiz:
             paired_rank = paired_hole_ranks[0]
             num_outs = sum(1 for card in self.deck if card[0] == paired_rank)
             cum_prob = 1
-            for i in range(5 - num_community_cards):
+            for i in range(2):
                 cum_prob *= (47 - num_outs - i)/(47 - i)
             probabilities['Three of a Kind'] = round((1 - cum_prob) * 100, 2)
         
         # Flush calculation (only when using at least one hole card)
         for hole_suit in hole_suits:
             suit_count = suit_counts[hole_suit]
-            if suit_count >= 4:  # Need one more for flush
+            if suit_count == 4:  # Need one more for flush
                 num_outs = sum(1 for card in self.deck if card[1] == hole_suit)
                 cum_prob = 1
-                for i in range(5 - num_community_cards):
+                for i in range(2):
                     cum_prob *= (47 - num_outs - i)/(47 - i)
                 probabilities['Flush'] = round((1 - cum_prob) * 100, 2)
                 break
@@ -107,10 +114,33 @@ class PokerQuiz:
         # Straight calculation (only counting straights using at least one hole card)
         straight_outs = self.count_straight_outs_post_flop(all_cards, hole_cards)
         if straight_outs > 0:
-            cum_prob = 1
-            for i in range(5 - num_community_cards):
-                cum_prob *= (47 - straight_outs - i)/(47 - i)
-            probabilities['Straight'] = round((1 - cum_prob) * 100, 2)
+            # Calculate probability of hitting at least one of the needed cards
+            # Probability of hitting at least one of the outs in two draws
+            prob_hitting_one = 1 - ((47 - straight_outs) / 47) * ((46 - straight_outs) / 46)
+            probabilities['Straight'] = round(prob_hitting_one * 100, 2)
+        return probabilities
+
+    def calculate_draw_probability(self, outs, cards_needed, deck_size):
+        """Calculate probability of hitting a draw"""
+        if cards_needed == 1:
+            return (outs / deck_size) * 100
+        elif cards_needed == 2:
+            return (outs / deck_size) * ((outs - 1) / (deck_size - 1)) * 100
+        return 0
+
+    def count_straight_outs(self, hole_cards):
+        """Count number of cards that could complete a straight"""
+        rank1, rank2 = self.rank_values[hole_cards[0][0]], self.rank_values[hole_cards[1][0]]
+        needed_ranks = set()
+        
+        # Find all possible straights containing both cards
+        for i in range(max(0, min(rank1, rank2) - 4), 
+                      min(len(self.ranks) - 4, max(rank1, rank2) + 1)):
+            straight_ranks = set(range(i, i + 5))
+            if rank1 in straight_ranks and rank2 in straight_ranks:
+                needed_ranks.update(straight_ranks - {rank1, rank2})
+        
+        return sum(1 for card in self.deck if self.rank_values[card[0]] in needed_ranks)
 
     def count_straight_outs_post_flop(self, all_cards, hole_cards):
         """Count straight outs after the flop, requiring at least one hole card"""
@@ -135,6 +165,42 @@ class PokerQuiz:
             total_outs += sum(1 for card in self.deck if self.rank_values[card[0]] == rank)
         
         return total_outs
+
+    def count_straight_flush_outs(self, hole_cards):
+        """Count number of cards that could complete a straight flush"""
+        suit = hole_cards[0][1]  # Both cards have same suit for straight flush possibility
+        straight_outs = self.count_straight_outs(hole_cards)
+        return sum(1 for card in self.deck if card[1] == suit and 
+                  self.rank_values[card[0]] in range(min(self.rank_values[hole_cards[0][0]], 
+                  self.rank_values[hole_cards[1][0]]) - 4, 
+                  max(self.rank_values[hole_cards[0][0]], self.rank_values[hole_cards[1][0]]) + 5))
+
+    def calculate_straight_probability(self, straight_outs):
+        """Calculate probability of making a straight"""
+        return (straight_outs / 47) * ((straight_outs - 1) / 46) * ((straight_outs - 2) / 45) * 100
+
+    def calculate_straight_flush_probability(self, straight_flush_outs):
+        """Calculate probability of making a straight flush"""
+        return (straight_flush_outs / 47) * ((straight_flush_outs - 1) / 46) * ((straight_flush_outs - 2) / 45) * 100
+
+    def calculate_royal_flush_probability(self, hole_cards):
+        """Calculate probability of making a royal flush"""
+        suit = hole_cards[0][1]
+        needed_ranks = {'10', 'J', 'Q', 'K', 'A'} - {hole_cards[0][0], hole_cards[1][0]}
+        royal_outs = sum(1 for card in self.deck if card[1] == suit and card[0] in needed_ranks)
+        return (royal_outs / 47) * ((royal_outs - 1) / 46) * ((royal_outs - 2) / 45) * 100
+
+    def calculate_full_house_probability(self, hole_cards):
+        """Calculate probability of making a full house"""
+        if hole_cards[0][0] == hole_cards[1][0]:
+            # Already have a pair
+            remaining_ranks = set(card[0] for card in self.deck)
+            trip_prob = len([c for c in self.deck if c[0] == hole_cards[0][0]]) / 47
+            pair_prob = sum(len([c for c in self.deck if c[0] == r]) / 47 for r in remaining_ranks)
+            return trip_prob * pair_prob * 100
+        else:
+            # Need to make two pair and then boat
+            return (6 / 47) * (5 / 46) * (4 / 45) * 100
 
     def quiz(self):
         """Run the poker probability quiz"""
@@ -169,15 +235,15 @@ class PokerQuiz:
                         difference = abs(guess - actual_prob)
                         self.total_questions += 1
                         
-                        print(f"Actual: {actual_prob}%")
+                        print(f"The actual probability is {actual_prob}%")
                         if difference <= 2:
-                            print("Correct! Within 2%.")
+                            print("Excellent! Within 2% of the actual probability.")
                             self.correct_answers += 1
                         else:
-                            print("Off by more than 2%.")
+                            print("Keep practicing! That was off by more than 2%.")
                         break
                     except ValueError:
-                        print("Enter a number or 'exit'")
+                        print("Please enter a valid number or 'exit'")
         return True
 
     def show_summary(self):
