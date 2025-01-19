38273d1033e420269d4c3a7883e58c03e177d7b7
Brad Levin <bradlevin08@gmail.com>
2025-01-19 17:29:02 -0500

    adding

--- validator.py
+++ validator.py
@@ -30,8 +30,8 @@ class MonteCarloValidator:
             num_to_deal = 5 - len(community_cards) if community_cards else 3
             flop = random.sample(deck, num_to_deal)
             all_cards = hole_cards + (community_cards if community_cards else []) + flop
-            
             # Check each hand type
+            
             for hand_type in hand_types:
                 if self._has_hand(all_cards, hand_type):
                     successes[hand_type] += 1
@@ -161,8 +161,73 @@ class MonteCarloValidator:
                 if count >= 4 and rank in hole_ranks:
                     return True
             return False
-        
         return False
+    def _num_outs(self, hole_cards, community_cards, target_hand):
+        """Calculate number of outs to achieve the target hand
+        Args:
+            cards_needed: 1 or 2, number of cards needed to complete the hand
+        """
+        # Create deck and remove known cards
+        deck = [[rank, suit] for rank in self.quiz.ranks for suit in self.quiz.suits]
+        for card in hole_cards + community_cards:
+            if card in deck:
+                deck.remove(card)
+        outs = 0
+        if len(community_cards) == 4:
+            # Single card outs
+            for potential_card in deck:
+                all_cards = hole_cards + community_cards + [potential_card]
+                if self._has_hand(all_cards, target_hand):
+                    outs += 1
+        else:
+            # For two cards, we want to count individual cards that could help
+            for card in deck:
+                # Check if this card could be part of a winning hand with any other remaining card
+                helps_make_hand = False
+                remaining_deck = deck.copy()
+                remaining_deck.remove(card)
+                
+                for second_card in remaining_deck:
+                    all_cards = hole_cards + community_cards + [card, second_card]
+                    if self._has_hand(all_cards, target_hand):
+                        helps_make_hand = True
+                        break
+                
+                if helps_make_hand:
+                    outs += 1
+
+        return outs
+
+    def calculate_outs(self, hole_cards, community_cards):
+        """Calculate outs for all possible hand improvements
+        Returns:
+            dict: Dictionary with keys as hand types and values as tuples (outs, probability)
+        """
+        hand_types = ["Pair", "Two Pair", "Three of a Kind", "Straight", 
+                      "Flush", "Full House", "Four of a Kind"]
+
+        outs_dict = {}
+        current_cards = hole_cards + community_cards
+        cards_needed = 1 if len(community_cards) == 4 else 2
+        remaining_cards = 52 - len(current_cards)
+
+        for hand_type in hand_types:
+            if not self._has_hand(current_cards, hand_type):
+                num_outs = self._num_outs(hole_cards, community_cards, hand_type)
+                # Calculate probability based on number of cards to come
+                if cards_needed == 1:
+                    # One card to come: outs / remaining cards
+                    probability = (num_outs / remaining_cards) * 100
+                else:
+                    # Two cards to come using binomial probability:
+                    # P(at least one out) = 1 - P(no outs)
+                    # P(no outs) = (remaining_cards - outs) * (remaining_cards - outs - 1) / (remaining_cards * (remaining_cards - 1))
+                    no_outs_prob = ((remaining_cards - num_outs) * (remaining_cards - num_outs - 1)) / (remaining_cards * (remaining_cards - 1))
+                    probability = (1 - no_outs_prob) * 100
+                
+                outs_dict[hand_type] = (num_outs, round(probability, 2))
+        return outs_dict
+
 
 def run_validation_tests():
     validator = MonteCarloValidator()
@@ -170,13 +235,16 @@ def run_validation_tests():
 if __name__ == "__main__":
     # run_validation_tests()
     validator = MonteCarloValidator()
-    with open('test_scenarios.json', 'r') as f:
-        test_scenarios = json.load(f)
-    all_probabilities = {}
-    for hand_type in test_scenarios:
-        all_probabilities[hand_type] = {}
-        for scenario in test_scenarios[hand_type]:
-            mc_prob = validator.simulate_post_flop(test_scenarios[hand_type][scenario]["hole_cards"], test_scenarios[hand_type][scenario]["community_cards"])
-            all_probabilities[hand_type][scenario] = mc_prob[hand_type]
-    print(all_probabilities)
+    # with open('test_scenarios.json', 'r') as f:
+    #     test_scenarios = json.load(f)
+    # all_probabilities = {}
+    # for hand_type in test_scenarios:
+    #     all_probabilities[hand_type] = {}
+    #     for scenario in test_scenarios[hand_type]:
+    #         mc_prob = validator.simulate_post_flop(test_scenarios[hand_type][scenario]["hole_cards"], test_scenarios[hand_type][scenario]["community_cards"])
+    #         all_probabilities[hand_type][scenario] = mc_prob[hand_type]
+    # print(all_probabilities)
     # pickle.dump(pair_probabilities, open('pair_probabilities.pickle', 'wb'))
+    hole_cards =  [["A", "♠"], ["7", "♥"]]
+    community_cards = [["K", "♦"], ["Q", "♣"], ["7", "♣"], ["J", "♦"]]
+    print(validator.calculate_outs(hole_cards, community_cards))
